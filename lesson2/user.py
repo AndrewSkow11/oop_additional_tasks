9b3422d726b713c5e017874ce1f52215045b9b60
Andrew Skow <skovorodnikov11@gmail.com>
2023-11-15 06:44:08 +0300

    lesson 2 - completed

--- lesson2/user.py
+++ lesson2/user.py
@@ -14,7 +14,48 @@
 
 
 class User:
-    pass
+
+    def __init__(self, name, password):
+        """Конструктор, принимающий имя пользователя и пароль"""
+        self.__name = name
+        self.__password = password
+        self._is_admin = False
+        self._is_logged_in = True
+
+    @property
+    def name(self):
+        """свойство, которое возвращает имя пользователя"""
+        return self.__name
+
+    @property
+    def password(self):
+        """Cвойство, которое позволяет установить или изменить
+         пароль пользователя"""
+        return self.__password
+
+    @password.setter
+    def password(self, password):
+        self.__password = password
+
+    @property
+    def is_admin(self):
+        """свойство, которое возвращает, является ли пользователь
+        администратором или нет"""
+        return self._is_admin
+
+    def login(self, password):
+        """метод, который проверяет, соответствует ли введенный
+        пароль паролю пользователя"""
+        print(password == self.__password)
+
+
+    def logout(self):
+        """метод, который выходит из аккаунта пользователя(устанавливает
+        значение свойств __is_logged_in в False при условии, что пользователь
+        залогинен)"""
+        if self._is_logged_in:
+            self._is_logged_in = False
+
 
 
 user1 = User("Alice", "qwerty")
