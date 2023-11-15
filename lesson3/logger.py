7a0a340f5d879959a64fe5643fad7151db3d62b2
Andrew Skow <skovorodnikov11@gmail.com>
2023-11-15 17:18:11 +0300

    lesson3: counter, fraction, logger - ok

--- lesson3/logger.py
+++ lesson3/logger.py
@@ -8,7 +8,14 @@
 
 
 class Logger:
-    pass
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __call__(self, message):
+        self.message = message
+
+        with open(self.filename, 'w') as file:
+            file.write(self.message)
 
 
 logger = Logger("log.txt")
