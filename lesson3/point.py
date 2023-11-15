2eae3911551325686a628a91eeddf49e9f26de2c
Andrew Skow <skovorodnikov11@gmail.com>
2023-11-15 18:30:36 +0300

    point

--- lesson3/point.py
+++ lesson3/point.py
@@ -9,7 +9,18 @@
 
 
 class Point:
-    pass
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f'{Point.__name__}({self.x}, {self.y})'
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def __add__(self, other):
+        return self.x + other.x, self.y + other.y
 
 
 point1 = Point(1, 2)
@@ -19,3 +30,10 @@ print(str(point1))  # (1, 2)
 point2 = Point(3, 4)
 point3 = point1 + point2
 print(point3)  # (4, 6)
+
+# /usr/bin/python3 /Users/andrejskovorodnikov/Desktop/oop_additional_tasks/lesson3/point.py
+# Point(1, 2)
+# (1, 2)
+# (4, 6)
+#
+# Process finished with exit code 0
