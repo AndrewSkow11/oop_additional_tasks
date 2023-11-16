b3e6fc101164350f67252c6900d068c9a1603481
Andrew Skow <skovorodnikov11@gmail.com>
2023-11-16 18:22:36 +0300

    lesson 6 completed

--- lessson6/person.py
+++ lessson6/person.py
@@ -14,13 +14,20 @@
 
 
 class Person:
-    pass
+    __slots__ = ('name', 'age')
+
+
+class Employee(Person):
+    __slots__ = ('salary',)
+
+class Manager(Employee):
+     __slots__ = ('bonus',)
 
 
 person = Person()
 person.name = "John"
 person.age = 30
-person.salary = 5000  # raises AttributeError
+# person.salary = 5000  # raises AttributeError
 
 employee = Employee()
 employee.name = "Jane"
